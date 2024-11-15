27efbd9a6f908dc9c97818f9b4def7585181a01a
Foo-x <www.sand.x@gmail.com>
2024-11-15 22:47:42 +0900

    chore: update script

--- golinks.sh
+++ golinks.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 docker run --rm -d \
-  -u "$(id -u):$(id -g)" \
   -v $PWD/routes:/golinks/routes \
   -p 80:80 \
-  golinks | xargs docker logs
+  ghcr.io/foo-x/golinks | xargs docker logs
